4f1359f980e0dc817f4c05fd65ec01a5d257b3ae
Filip <101202559+ClanyX@users.noreply.github.com>
2025-01-03 14:08:39 +0100

    Create I2C_TEST.ino

---
+++ I2C_display/I2C_TEST/I2C_TEST.ino
@@ -0,0 +1,18 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Hello!");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() {
+  lcd.home();
+  lcd.print(millis() / 1000);
+  delay(1000);
+}
