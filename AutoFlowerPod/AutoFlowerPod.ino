ce4163998b73efc978f6ae87c97a20555b4594af
Filip <101202559+ClanyX@users.noreply.github.com>
2025-08-22 01:13:56 +0200

    Refactor animation logic and fix macro definitions
    Removed incorrect '=' from macro definitions and refactored animation handling for drink, sun, and cloud states. Replaced u8g calls with display object for drawing functions and moved animation loops into testDirt for improved display updates. Removed unused displayArt function and added a delay in enterSleep for stability.

--- AutoFlowerPod/AutoFlowerPod.ino
+++ AutoFlowerPod/AutoFlowerPod.ino
@@ -9,10 +9,10 @@
 #define pump 5
 
 ///define variables
-#define dryLevel = 500         //when the sensor checked
-#define pumpSpeed = 200        //level of analog signal to pump
-#define pumpTime = 5           //seconds
-#define interuptCounter = 150  //8 second times 150 = 20 min
+#define dryLevel 500        //when the sensor checked
+#define pumpSpeed 200       //level of analog signal to pump
+#define pumpTime 5          //seconds
+#define interuptCounter 150 //8 second times 150 = 20 min
 
 ///variables
 U8GLIB_SSD1306_128X64 display(U8G_I2C_OPT_NONE);
@@ -23,6 +23,7 @@ volatile byte cycleCount = 0;
 
 int sunState = 0;
 int cloudState = 0;
+int drinkState = 0;
 
 ///interupt
 ISR(WDT_vect) {
@@ -33,34 +34,7 @@ ISR(WDT_vect) {
   }
 }
 
-///functions
-void testDirt() {
-  humiditySensorData = analogRead(humiditySensor);
-
-  //check the data - dry level
-  if (humiditySensorData > dryLevel) {
-    //motor
-    motorRun();
-
-    //display
-    display.firstPage();
-    do {
-      displayArt();
-    } while (display.nextPage());
-  } else {
-    if (animationPick) {
-      drawSun(sunState);
-      animationPick = !animationPick;
-    } else {
-      drawCloud(cloudState);
-      animationPick = !animationPick;
-    }
-  }
-}
-
 //animation drink
-int drinkState = 0;
-
 void drawStraw(int state) {
   int x1 = 70;
   int y1 = 20;
@@ -80,13 +54,13 @@ void drawStraw(int state) {
         break;
     }
   }
-  u8g.drawLine(x1, y1, x2, y2);
+  display.drawLine(x1, y1, x2, y2);
 }
 
 void drawDrinkState(int state) {
-  u8g.drawRFrame(44, 20, 40, 40, 5);  // Tělo skleničky
-  u8g.drawBox(48, 18, 32, 2);         // Víčko
-  u8g.drawHLine(46, 62, 36);          // Stín
+  display.drawRFrame(44, 20, 40, 40, 5);  // Tělo skleničky
+  display.drawBox(48, 18, 32, 2);         // Víčko
+  display.drawHLine(46, 62, 36);          // Stín
 
   // Vykreslení animovaného brčka
   drawStraw(state);
@@ -118,7 +92,7 @@ void drawDrinkState(int state) {
       break;
   }
 
-  u8g.drawBox(44, drinkLevelY, 40, drinkLevelHeight);
+  display.drawBox(44, drinkLevelY, 40, drinkLevelHeight);
 
   // Animace bublinek
   int bubbleY1 = 0;
@@ -156,18 +130,10 @@ void drawDrinkState(int state) {
   }
 
   if (state < 4) {
-    u8g.drawCircle(55, bubbleY1, 1);
-    u8g.drawCircle(68, bubbleY2, 1);
-    u8g.drawCircle(60, bubbleY3, 1);
-    u8g.drawCircle(73, bubbleY4, 1);
-  }
-
-  animationState++;
-  if (animationState > 4) {
-    animationState = 0;
-  } else {
-    delay(625);
-    drawDrinkState(state);
+    display.drawCircle(55, bubbleY1, 1);
+    display.drawCircle(68, bubbleY2, 1);
+    display.drawCircle(60, bubbleY3, 1);
+    display.drawCircle(73, bubbleY4, 1);
   }
 }
 //
@@ -180,7 +146,7 @@ void drawSun(int state) {
   int radius = 15;
 
   // Tělo sluníčka (plný kruh)
-  u8g.drawDisc(centerX, centerY, radius);
+  display.drawDisc(centerX, centerY, radius);
 
   // Délka paprsků
   int rayLength = 5;
@@ -207,27 +173,19 @@ void drawSun(int state) {
   }
 
   // Svislé a vodorovné paprsky
-  u8g.drawLine(centerX - radius - offset, centerY, centerX - radius - offset - rayLength, centerY);  // Vlevo
-  u8g.drawLine(centerX + radius + offset, centerY, centerX + radius + offset + rayLength, centerY);  // Vpravo
-  u8g.drawLine(centerX, centerY - radius - offset, centerX, centerY - radius - offset - rayLength);  // Nahoru
-  u8g.drawLine(centerX, centerY + radius + offset, centerX, centerY + radius + offset + rayLength);  // Dolů
+  display.drawLine(centerX - radius - offset, centerY, centerX - radius - offset - rayLength, centerY);  // Vlevo
+  display.drawLine(centerX + radius + offset, centerY, centerX + radius + offset + rayLength, centerY);  // Vpravo
+  display.drawLine(centerX, centerY - radius - offset, centerX, centerY - radius - offset - rayLength);  // Nahoru
+  display.drawLine(centerX, centerY + radius + offset, centerX, centerY + radius + offset + rayLength);  // Dolů
 
   // Úhlopříčné paprsky
   int diagX = (radius * 0.707);
   int diagY = (radius * 0.707);
 
-  u8g.drawLine(centerX - diagX - offset, centerY - diagY - offset, centerX - diagX - offset - rayLength, centerY - diagY - offset - rayLength);  // Horní levý
-  u8g.drawLine(centerX + diagX + offset, centerY - diagY - offset, centerX + diagX + offset + rayLength, centerY - diagY - offset - rayLength);  // Horní pravý
-  u8g.drawLine(centerX - diagX - offset, centerY + diagY + offset, centerX - diagX - offset - rayLength, centerY + diagY + offset + rayLength);  // Dolní levý
-  u8g.drawLine(centerX + diagX + offset, centerY + diagY + offset, centerX + diagX + offset + rayLength, centerY + diagY + offset + rayLength);  // Dolní pravý
-
-  animationState++;
-  if (animationState > 4) {
-    animationState = 0;
-  } else {
-    delay(200);
-    drawSun(state);
-  }
+  display.drawLine(centerX - diagX - offset, centerY - diagY - offset, centerX - diagX - offset - rayLength, centerY - diagY - offset - rayLength);  // Horní levý
+  display.drawLine(centerX + diagX + offset, centerY - diagY - offset, centerX + diagX + offset + rayLength, centerY - diagY - offset - rayLength);  // Horní pravý
+  display.drawLine(centerX - diagX - offset, centerY + diagY + offset, centerX - diagX - offset - rayLength, centerY + diagY + offset + rayLength);  // Dolní levý
+  display.drawLine(centerX + diagX + offset, centerY + diagY + offset, centerX + diagX + offset + rayLength, centerY + diagY + offset + rayLength);  // Dolní pravý
 }
 //
 
@@ -256,20 +214,68 @@ void drawCloud(int state) {
   }
 
   // Vykreslení mráčku (čtyři kruhy, které se překrývají)
-  u8g.drawDisc(cloudX, cloudY + cloudOffsetY, 15);
-  u8g.drawDisc(cloudX + 20, cloudY + 5 + cloudOffsetY, 12);
-  u8g.drawDisc(cloudX + 40, cloudY + cloudOffsetY, 15);
-  u8g.drawDisc(cloudX + 20, cloudY - 10 + cloudOffsetY, 10);
-
-  animationState++;
-  if (animationState > 3) {
-    animationState = 0;
-  } else {
-    delay(250);
-    drawCloud(state);
-  }
+  display.drawDisc(cloudX, cloudY + cloudOffsetY, 15);
+  display.drawDisc(cloudX + 20, cloudY + 5 + cloudOffsetY, 12);
+  display.drawDisc(cloudX + 40, cloudY + cloudOffsetY, 15);
+  display.drawDisc(cloudX + 20, cloudY - 10 + cloudOffsetY, 10);
 }
 //
+
+///functions
+void testDirt() {
+  humiditySensorData = analogRead(humiditySensor);
+
+  //check the data - dry level
+  if (humiditySensorData > dryLevel) {
+    //motor
+    motorRun();
+
+    //display
+    while (drinkState < 5) {
+      display.firstPage();
+      do {
+        drawDrinkState(drinkState);
+      } while (display.nextPage());
+      drinkState++;
+      delay(625);
+    }
+    drinkState = 0;
+    display.firstPage();
+    do {
+    } while (display.nextPage());
+  } else {
+    if (animationPick) {
+      while (sunState < 5) {
+        display.firstPage();
+        do {
+          drawSun(sunState);
+        } while (display.nextPage());
+        sunState++;
+        delay(200);
+      }
+      sunState = 0;
+      display.firstPage();
+      do {
+      } while (display.nextPage());
+
+      animationPick = !animationPick;
+    } else {
+      while (cloudState < 4) {
+        display.firstPage();
+        do {
+          drawCloud(cloudState);
+        } while (display.nextPage());
+        cloudState++;
+        delay(250);
+      }
+      cloudState = 0;
+      display.firstPage();
+      do {
+      } while (display.nextPage());
+      animationPick = !animationPick;
+    }
+  }
+}
 
 void enterSleep() {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
@@ -278,6 +284,7 @@ void enterSleep() {
   //Here will program continue after the interupt stack 150 times
 
   power_all_enable();
+  delay(100);
 }
 
 void setupWatchDog() {
@@ -292,13 +299,6 @@ void motorRun() {
   analogWrite(pump, pumpSpeed);
   delay(pumpTime * 1000);
   analogWrite(pump, 0);
-}
-
-void displayArt() {
-  drawDrinkState(drinkState);
-
-  display.setFont(u8g_font_unifont);
-  display.drawStr(0, 20, humiditySensorData);
 }
 
 void setup() {
